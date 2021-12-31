4bd1c8dffcb4235f19824c7790eb3484991cf149
toutou1993 <botb60413@gmail.com>
2021-12-31 16:56:15 +0100

    sendgenericttemplate

--- processes/messages.js
+++ processes/messages.js
@@ -30,7 +30,7 @@ module.exports = function processMessage(event) {
                 senderAction(senderID);
                 // after the response is recieved we will send the details in a Generic template
 
-                sendGenericTemplate(senderID, body);
+                // sendGenericTemplate(senderID, body);
             });
         }
     }
