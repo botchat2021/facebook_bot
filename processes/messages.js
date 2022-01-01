5946641bbf7e54caae8fc54aaeacc242cf69b8e1
toutou1993 <botb60413@gmail.com>
2022-01-01 14:57:00 +0100

    messeages.js change

--- processes/messages.js
+++ processes/messages.js
@@ -2,6 +2,7 @@ const request = require('request');
 const senderAction = require('../templates/senderAction');
 const sendMessage = require('../templates/sendMessage');
 const sendGenericTemplate = require('../templates/sendGenericTemplate');
+const { response } = require('express');
 module.exports = function processMessage(event) {
     if (!event.message.is_echo) {
         const message = event.message;
@@ -10,28 +11,12 @@ module.exports = function processMessage(event) {
         console.log("Message is: " + JSON.stringify(message));
         if (message.text) {
             // now we will take the text received and send it to an food tracking API.
-            let text = message.text;
-            let request = require("request");
-            let options = {
-                method: 'POST',
-                url: 'https://mefit-preprod.herokuapp.com/api/getnutritionvalue',
-                headers: {
-                    'cache-control': 'no-cache',
-                    'content-type': 'application/json'
-                },
-                body: {
-                    userID: process.env.USERID,
-                    searchTerm: text
-                },
-                json: true
-            };
-            request(options, function(error, response, body) {
-                if (error) throw new Error(error);
-                senderAction(senderID);
-                // after the response is recieved we will send the details in a Generic template
-
-                sendGenericTemplate(senderID, body);
-            });
+            // let text = message.text;
+            response = {
+                "text": `You sent the message: "${received_message.text}". Now send me an image!`
+            }
+            senderAction(senderID);
+            sendMessage(senderID, response);
         }
     }
 }
