65cd455efb50e82e5b4e104c02e7743c20e60d39
toutou1993 <botb60413@gmail.com>
2022-01-02 14:38:05 +0100

    deploy3

--- processes/messages.js
+++ processes/messages.js
@@ -12,11 +12,27 @@ module.exports = function processMessage(event) {
         if (message.text) {
             // now we will take the text received and send it to an food tracking API.
             // let text = message.text;
-            let response = {
-                "text": `You sent the message: "${message.text}". Now send me an image!`
-            }
-            senderAction(senderID);
-            sendMessage(senderID, response);
+            let text = message.text;
+            let request = require("request");
+            let options = {
+                method: 'GET',
+                url: 'http://127.0.0.1:5000/flask/' + text,
+                headers: {
+                    'cache-control': 'no-cache',
+                    'content-type': 'application/json'
+                },
+                body: {
+                    text: msg
+                },
+                json: true
+            };
+            request(options, function(error, response, body) {
+                if (error) throw new Error(error);
+                senderAction(senderID);
+
+
+                sendMessage(senderID, body);
+            });
         }
     }
 }
