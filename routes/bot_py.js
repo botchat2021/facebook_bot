efaacac6b34f1f5559d73edda7b3af99008da492
toutou1993 <botb60413@gmail.com>
2022-01-10 18:33:10 +0100

    bot_py fetch

--- routes/bot_py.js
+++ routes/bot_py.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 module.exports = function botApi(received_message) {
     const text = received_message.text;
     const Url = 'http://127.0.0.1:5000/flask/' + text;
-
+    var obj;
     fetch(Url).then(res => res.json())
         .then(data => obj = data)
         .then(() => console.log(obj));
